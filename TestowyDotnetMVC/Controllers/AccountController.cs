0cac93b5f6cea17915c5716a80a3a2041c10f2a2
Mateusz Atłasik <mateusz.atlasik@gmail.com>
2024-07-08 14:57:12 +0200

    Inicjalny

---
+++ TestowyDotnetMVC/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class AccountController : Controller
+{
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+}
